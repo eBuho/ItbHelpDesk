d6807e7658cd7aedd985495c916519e8237cd88e
Javier Condor <javier7co@gmail.com>
2013-09-20 10:48:09 -0500

    Modifique el ubigeo porque debe entenderse que es padre o superior

--- grails-app/domain/pe/itb/comunes/Ubigeo.groovy
+++ grails-app/domain/pe/itb/comunes/Ubigeo.groovy
@@ -8,14 +8,14 @@ class Ubigeo {
 	String usuCrea
 	Date lastUpdated
 	String usuModifica
-	Ubigeo ubigeo
+	Ubigeo ubigeoSup
 	
     static constraints = {
-		codUbigeo(blank: false, matches:"[0-9]+" ,maxSize:9, unique:true, nullable: false)
+		codUbigeo(blank: false, matches:"[0-9]+" ,maxSize:6, unique:true, nullable: false)
 		desUbigeo(blank: false, maxSize:100, nullable: false)
 		usuCrea(maxSize:15, blank: true)
 		usuModifica(maxSize:15, blank: true)		
-		ubigeo(blank: true, nullable:true)
+		ubigeoSup(blank: true, nullable:true)
     }
 	
 	String toString(){
