d2f948aa1b1c20253e86c34d59d3c3a73ef3bb57
Javier Condor <javier7co@gmail.com>
2013-09-20 19:12:41 -0500

    Clase Javier modificada Termine mis cambios

--- grails-app/domain/pe/itb/comunes/Javier.groovy
+++ grails-app/domain/pe/itb/comunes/Javier.groovy
@@ -3,11 +3,13 @@ package pe.itb.comunes
 class Javier {
 
 	String nombre
+	String edad
+	String apellido
 	
     static constraints = {
     }
     
     String toString(){
-		nombre
+		nombre + apellido
 	}
 }
